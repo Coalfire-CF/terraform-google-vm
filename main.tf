bacafeb950e566cfaed7cf1e17f9c7b8cd1804fd
Michael Scribellito <mscribellito@gmail.com>
2023-10-03 22:32:16 -0400

    Initial Release

---
+++ main.tf
@@ -0,0 +1,114 @@
+locals {
+  startup_scripts = var.startup_scripts == null ? null : [
+    for script in var.startup_scripts : templatefile(
+      script.path,
+      try(script.vars, {})
+    )
+  ]
+}
+
+/*************************************************
+ Snapshot schedule
+*************************************************/
+
+resource "google_compute_disk_resource_policy_attachment" "policy_attachment" {
+  count   = var.snapshot_schedule != null ? var.num_instances : 0
+  project = var.project_id
+
+  name = var.snapshot_schedule
+  disk = google_compute_instance.compute_instance[count.index].name
+  zone = element(var.zones, count.index)
+}
+
+/*************************************************
+ Instances
+*************************************************/
+
+resource "google_compute_instance" "compute_instance" {
+  count   = var.num_instances
+  project = var.project_id
+
+  name     = var.num_instances == 1 ? var.name : "${var.name}${count.index + 1}"
+  hostname = var.num_instances == 1 ? "${var.name}.${var.domain_name}" : "${var.name}.${count.index + 1}${var.domain_name}"
+
+  machine_type     = var.machine_type
+  min_cpu_platform = var.min_cpu_platform
+  can_ip_forward   = var.can_ip_forward
+
+  boot_disk {
+    auto_delete = var.auto_delete
+    initialize_params {
+      size  = var.disk_size_gb
+      type  = var.disk_type
+      image = var.source_image
+    }
+    kms_key_self_link = var.disk_encryption_key
+    #checkov:skip=CKV_GCP_39:Shielded VM enablement is unnecessary
+  }
+
+  zone = element(var.zones, count.index)
+
+  network_interface {
+    network            = var.network
+    subnetwork         = var.subnetwork
+    subnetwork_project = var.subnetwork_project
+    network_ip         = length(var.network_ip) > 0 ? try(var.network_ip[count.index], var.network_ip) : null
+    dynamic "access_config" {
+      #checkov:skip=CKV_GCP_40:VMs have public IP addresses
+      for_each = var.access_config
+      content {
+        nat_ip       = try(access_config.value.nat_ip[count.index], access_config.value.nat_ip)
+        network_tier = access_config.value.network_tier
+      }
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = var.additional_networks
+    content {
+      network            = network_interface.value.network
+      subnetwork         = network_interface.value.subnetwork
+      subnetwork_project = network_interface.value.subnetwork_project
+      network_ip         = length(network_interface.value.network_ip) > 0 ? try(network_interface.value.network_ip[count.index], network_interface.value.network_ip) : null
+      dynamic "access_config" {
+        for_each = network_interface.value.access_config
+        content {
+          nat_ip       = access_config.value.nat_ip
+          network_tier = access_config.value.network_tier
+        }
+      }
+    }
+  }
+
+  deletion_protection = var.deletion_protection
+
+  labels = var.labels
+
+  metadata = merge(var.metadata, {
+    #checkov:skip=CKV_GCP_32:Project-wide SSH keys are necessary 
+    windows-startup-script-ps1 = local.startup_scripts == null ? null : (var.windows == true ? join("\n", local.startup_scripts) : null)
+    startup-script             = local.startup_scripts == null ? null : (var.windows == false ? join("\n", local.startup_scripts) : null)
+  })
+
+  dynamic "service_account" {
+    for_each = var.service_account == null ? [] : [var.service_account]
+    content {
+      email  = lookup(service_account.value, "email", null)
+      scopes = lookup(service_account.value, "scopes", null)
+    }
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      # do not clear out any windows passwords and ssh keys
+      metadata["windows-keys"],
+      metadata["ssh-keys"],
+      # do not recreate instance when newer image available
+      boot_disk.0.initialize_params.0.image,
+      # do not touch resource policies
+      resource_policies
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,201 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+  default     = null
+}
+
+variable "num_instances" {
+  type        = number
+  description = "Number of instances to create."
+  default     = 1
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Machine type to create, e.g. n1-standard-1"
+  default     = "n1-standard-1"
+}
+
+variable "min_cpu_platform" {
+  type        = string
+  description = "Specifies a minimum CPU platform. Applicable values are the friendly names of CPU platforms, such as Intel Haswell or Intel Skylake. See the complete list: https://cloud.google.com/compute/docs/instances/specify-min-cpu-platform"
+  default     = null
+}
+
+variable "can_ip_forward" {
+  type        = bool
+  description = "Enable IP forwarding, for NAT instances for example"
+  default     = false
+}
+
+variable "zones" {
+  type        = list(string)
+  description = "Zones where the instances should be created. If not specified, instances will be spread across available zones in the region."
+  default     = null
+}
+
+variable "name" {
+  type        = string
+  description = "Name of instances"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name of instances, prefixed by '.'"
+  default     = null
+}
+
+variable "deletion_protection" {
+  type        = bool
+  description = "Enable deletion protection on this instance. Note: you must disable deletion protection before removing the resource, or the instance cannot be deleted and the Terraform run will not complete successfully."
+  default     = false
+}
+
+variable "tags" {
+  type        = list(string)
+  description = "Network tags, provided as a list"
+  default     = []
+}
+
+variable "labels" {
+  type        = map(string)
+  description = "Labels, provided as a map"
+  default     = {}
+}
+
+variable "windows" {
+  type        = bool
+  description = "True if Windows VM"
+  default     = false
+}
+
+###########################
+# Public IP
+###########################
+variable "access_config" {
+  description = "Access configurations, i.e. IPs via which the VM instance can be accessed via the Internet."
+  type = list(object({
+    nat_ip       = any
+    network_tier = string
+  }))
+  default = []
+}
+
+###########
+# metadata
+###########
+
+variable "metadata" {
+  type        = map(string)
+  description = "Metadata, provided as a map"
+  default     = {}
+}
+
+variable "startup_scripts" {
+  type = list(object({
+    path = string,
+    vars = map(string)
+  }))
+  description = "User startup scripts to run when instances spin up"
+  default     = null
+}
+
+##################
+# service_account
+##################
+
+variable "service_account" {
+  type = object({
+    email  = string
+    scopes = set(string)
+  })
+  description = "Service account to attach to the instance. See https://www.terraform.io/docs/providers/google/r/compute_instance_template.html#service_account."
+}
+
+#######
+# disk
+#######
+
+variable "source_image" {
+  type        = string
+  description = "Source disk image. If neither source_image nor source_image_family is specified, defaults to the latest public CentOS image."
+  default     = ""
+}
+
+variable "disk_labels" {
+  type        = map(string)
+  description = "Labels to be assigned to boot disk, provided as a map"
+  default     = {}
+}
+
+variable "disk_size_gb" {
+  type        = number
+  description = "Boot disk size in GB"
+  default     = 100
+}
+
+variable "disk_type" {
+  type        = string
+  description = "Boot disk type, can be either pd-ssd, local-ssd, or pd-standard"
+  default     = "pd-ssd"
+}
+
+variable "disk_encryption_key" {
+  type        = string
+  description = "The self_link of the encryption key that is stored in Google Cloud KMS to encrypt this disk."
+}
+
+variable "snapshot_schedule" {
+  type        = string
+  description = "Name of snapshot schedule"
+  default     = null
+}
+
+variable "auto_delete" {
+  type        = bool
+  description = "Whether or not the boot disk should be auto-deleted"
+  default     = true
+}
+
+####################
+# network_interface
+####################
+
+variable "subnetwork" {
+  type        = string
+  description = "The name of the subnetwork to attach this interface to. The subnetwork must exist in the same region this instance will be created in. Either network or subnetwork must be provided."
+  default     = ""
+}
+
+variable "subnetwork_project" {
+  type        = string
+  description = "The ID of the project in which the subnetwork belongs. If it is not provided, the provider project is used."
+  default     = ""
+}
+
+variable "network" {
+  type        = string
+  description = "The name or self_link of the network to attach this interface to. Use network attribute for Legacy or Auto subnetted networks and subnetwork for custom subnetted networks."
+  default     = ""
+}
+
+variable "network_ip" {
+  type        = any
+  description = "Private IP address to assign to the instance if desired."
+  default     = ""
+}
+
+variable "additional_networks" {
+  description = "Additional network interface details for GCE, if any."
+  default     = []
+  type = list(object({
+    network            = string
+    subnetwork         = string
+    subnetwork_project = string
+    network_ip         = any
+    access_config = list(object({
+      nat_ip       = string
+      network_tier = string
+    }))
+  }))
+}
